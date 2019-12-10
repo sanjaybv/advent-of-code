3985e80a7bfd941fc0909ee59aa733da6478dbf2
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2019-12-10 09:24:49 -0500

    Add 06 part 2

--- 2019/06/main.py
+++ 2019/06/main.py
@@ -2,16 +2,26 @@ from fileinput import input
 from functools import lru_cache
 
 edges = map(lambda x: tuple(x.strip().split(')')), input())
-
-# map from a node to its parent
-nodes = {b: a for (a, b) in edges}
+nodes = {b: a for (a, b) in edges} # map from a node to its parent
 
 @lru_cache(maxsize=None)
 def orbits(node):
     parent = nodes.get(node, "")
     return 0 if parent == "" else orbits(parent) + 1
 
-
 print(sum(orbits(node) for node in nodes.keys()))
 
+def ancestors(node):
+    anc = []
+    while node in nodes:
+        anc.append(nodes[node])
+        node = nodes[node]
+    return anc
 
+def common_ancestor():
+    for i, y in enumerate(ancestors("YOU")):
+        for j, s in enumerate(ancestors("SAN")):
+            if y == s:
+                return i + j
+
+print(common_ancestor())
