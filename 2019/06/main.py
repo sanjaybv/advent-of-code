ef1b513d5002b1f96ed8aed4301b80c483d779d4
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2019-12-10 09:02:39 -0500

    Modify part 1

--- 2019/06/main.py
+++ 2019/06/main.py
@@ -1,22 +1,17 @@
 from fileinput import input
 from functools import lru_cache
 
-class Node:
-    def __init__(self, parent=None):
-        self.parent = parent
-
 edges = map(lambda x: tuple(x.strip().split(')')), input())
 
-nodes = {}
-for a, b in edges:
-    nodes[a] = nodes.get(a, Node())
-    nodes[b] = nodes.get(b, Node())
-    nodes[b].parent = nodes[a]
+# map from a node to its parent
+nodes = {b: a for (a, b) in edges}
 
 @lru_cache(maxsize=None)
 def orbits(node):
-    return 0 if node.parent == None else orbits(node.parent) + 1
+    parent = nodes.get(node, "")
+    return 0 if parent == "" else orbits(parent) + 1
 
-print(sum(orbits(node) for node in nodes.values()))
+
+print(sum(orbits(node) for node in nodes.keys()))
 
 
