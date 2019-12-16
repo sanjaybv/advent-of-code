ee93f11bbd23fa0e2237a882c0b2e3c09277e29c
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2019-12-15 19:54:47 -0500

    Add part 2

--- 2019/08/main.py
+++ 2019/08/main.py
@@ -7,3 +7,12 @@ image = image.reshape(-1, 6, 25)
 i_0 = np.argmin(np.sum(image == 0, (1, 2)))
 print(np.sum(image[i_0] == 1) * np.sum(image[i_0] == 2))
 
+final = np.full(image.shape[1:], 2)
+for x in range(final.shape[0]):
+    for y in range(final.shape[1]):
+        for i in image[:, x, y]:
+            if i != 2:
+                final[x][y] = i
+                break
+
+print(final)
