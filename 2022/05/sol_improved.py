5446eb9a04098d1bb5ee1e46578f619e9616097d
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2022-12-04 23:46:33 -0800

    moar improvements day 5

--- 2022/05/sol_improved.py
+++ 2022/05/sol_improved.py
@@ -1,6 +1,7 @@
 import fileinput
 from itertools import tee
 from copy import deepcopy as dcopy
+import re
 
 lines = [l.rstrip('\n') for l in fileinput.input()]
 
@@ -12,22 +13,17 @@ for col in range(1, len(sl[-1]), 4):
     ))))
 
 
-ins =  map(
-            lambda parts: map(
-                lambda x: int(x[1]) if x[0] < 1 else int(x[1])-1, enumerate(filter(str.isnumeric, parts))
-            ), (l.split() for l in lines[lines.index('')+1:])
-       )
-ia, ib = tee(ins)
+ins = list(map(int, re.findall(r"\d+", l)) for l in lines[lines.index('')+1:])
 
 def solve(ins, s, func):
     for i in ins:
         a, f, t = list(i)
-        s[t].extend(func(s[f][-a:]))
-        s[f] = s[f][:-a]
+        s[t-1].extend(func(s[f-1][-a:]))
+        s[f-1] = s[f-1][:-a]
     print(''.join([j.pop() for j in s]))
 
 # part 1
-solve(ia, dcopy(stacks), reversed)
+solve(dcopy(ins), dcopy(stacks), reversed)
 
 # part 2
-solve(ia, dcopy(stacks), lambda x: x)
+solve(dcopy(ins), dcopy(stacks), lambda x: x)
