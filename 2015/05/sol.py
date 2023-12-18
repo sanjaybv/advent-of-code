7c86f508be7a5a0b735e451c1903437cc461b54f
Sanjay Bharadhwaj Vijayaraghava <sanjaybwaj@gmail.com>
2023-12-17 23:14:00 -0800

    add 2015/05

---
+++ 2015/05/sol.py
@@ -0,0 +1,30 @@
+import fileinput as fi
+
+
+def check(s):
+    if sum([1 for ch in s if ch in "aeiou"]) < 3:
+        return 0
+    if sum([1 for a, b in zip(s[:-1], s[1:]) if a == b]) < 1:
+        return 0
+    if sum([1 for bad in ["ab", "cd", "pq", "xy"] if bad in s]) > 0:
+        return 0
+    return 1
+
+
+def check_again(s):
+    m = {}
+    if sum([1 for i in range(len(s)-1) if s[i:i+2] in s[:i] or s[i:i+2] in s[i+2:]]) < 1:
+        return 0
+    if sum([1 for i in range(len(s)-2) if s[i] == s[i+2]]) < 1:
+        return 0
+
+    return 1
+
+
+inp = [li.strip() for li in fi.input()]
+
+c = sum([check(i) for i in inp])
+print(c)
+
+ca = sum([check_again(i) for i in inp])
+print(ca)
--- 2022/06/sol_improved.py
+++ 2022/06/sol_improved.py
@@ -1,11 +1,13 @@
 import fileinput
 from itertools import takewhile
 
-l = next(iter(fileinput.input()))
+inp = next(iter(fileinput.input()))
+
 
 def solve(n: int):
-    tw = takewhile(lambda i: len(set(l[i:i+n])) < n, range(0, len(l)-n))
+    tw = takewhile(lambda i: len(set(inp[i : i + n])) < n, range(0, len(inp) - n))
     return len(list(tw)) + n
+
 
 # part 1
 print(solve(4))
